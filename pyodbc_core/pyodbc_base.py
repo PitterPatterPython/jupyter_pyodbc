94d82dda8ed8aa2a3aa020cae8e01b291c065667
John Omernik <darkness@cybermac.local>
2021-04-14 14:58:25 -0500

    Update to use new doc system

--- pyodbc_core/pyodbc_base.py
+++ pyodbc_core/pyodbc_base.py
@@ -48,29 +48,18 @@ class Pyodbc(Integration):
     myopts['pyodbc_conn_default'] = ["default", 'Default instance name for connections']
     
     # Class Init function - Obtain a reference to the get_ipython()
-    def __init__(self, shell, pd_display_grid="html", pyodbc_conn_url_default="", debug=False, *args, **kwargs):
-        super(Pyodbc, self).__init__(shell, debug=debug, pd_display_grid=pd_display_grid) # Change the class name (Start) to match your actual class name
-        self.debug = debug
 
-        self.opts['pd_display_grid'][0] = pd_display_grid
-        if pd_display_grid == "qgrid":
-            try:
-                import qgrid
-            except:
-                print ("WARNING - QGRID SUPPORT FAILED - defaulting to html")
-                self.opts['pd_display_grid'][0] = "html"
+    def __init__(self, shell, debug=False, *args, **kwargs):
+        super(Pyodbc, self).__init__(shell, debug=debug)
+        self.debug = debug
 
         #Add local variables to opts dict
         for k in self.myopts.keys():
             self.opts[k] = self.myopts[k]
 
         self.load_env(self.custom_evars)
-        if pyodbc_conn_url_default != "":
-            if "default" in self.instances.keys():
-                print("Warning: default instance in ENV and passed to class creation - overwriting ENV")
-            self.fill_instance("default", pyodbc_conn_url_default)
-
         self.parse_instances()
+
 
     # We use a custom disconnect in pyodbc so we try to close the connection before nuking it
     def customDisconnect(self, instance):
@@ -222,9 +211,33 @@ class Pyodbc(Integration):
 
 
 # Display Help can be customized
-    def customHelp(self):
+    def customOldHelp(self):
         self.displayIntegrationHelp()
         self.displayQueryHelp("select * from mydatabase.mytable")
+
+
+    def retCustomDesc(self):
+        return "Jupyter integration for working with the PyODBC based data sources"
+
+
+    def customHelp(self, curout):
+        n = self.name_str
+        mn = self.magic_name
+        m = "%" + mn
+        mq = "%" + m
+        table_header = "| Magic | Description |\n"
+        table_header += "| -------- | ----- |\n"
+        out = curout
+        qexamples = []
+        qexamples.append(["myinstance", "select * from mydatabase.mytable", "Run a sql query against myinstance"])
+        qexamples.append(["", "select * from mydatabase.mytable", "Run a sql query against the default instance"])
+        out += self.retQueryHelp(qexamples)
+
+        return out
+
+
+
+
 
     def as_pandas_DataFrame(self):
         cursor = self.session
